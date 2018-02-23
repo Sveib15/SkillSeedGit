4ca559693c309ffeb346c9f1206ce9c20fd82482
Svei Bruce <sveinbruce132@gmail.com>
2018-02-23 10:18:38 +0100

    Setup

---
+++ Prosjekt-kopi/HHR/CalenderViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CalendarVC.swift
+//  HHR
+//
+//  Created by Anders Berntsen on 13.02.2018.
+//  Copyright © 2018 Helping Hand. All rights reserved.
+//
+//
+
+
+import UIKit
+
+class CalendarVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func closePopup(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    //nei
+    
+    
+    
+    
+    
+}
